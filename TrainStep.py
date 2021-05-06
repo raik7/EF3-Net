436bc6bd7cfdaaa833e39d1c4591d3865d083aca
raik7 <52767477+raik7@users.noreply.github.com>
2021-05-06 22:53:29 +0800

    Update TrainStep.py

--- TrainStep.py
+++ TrainStep.py
@@ -1,10 +1,6 @@
 import evaluate
-import evaluateo
-import evaluateiou
 
 def trainStep(model, X_train, Y_train, X_test, Y_test, epochs, batchSize, iters, results_save_path, losshistory, reverse = False):
-
-    
     for epoch in range(epochs):
         print('Epoch : {}'.format(epoch+1))
         model.fit(x=X_train, y=Y_train, batch_size=batchSize, epochs=1, verbose=1)

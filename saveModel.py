b9a1996792b6f1582acf4b7638b736b4b0cdb357
raik7 <52767477+raik7@users.noreply.github.com>
2022-03-07 10:16:54 +0800

    Delete saveModel.py

--- saveModel.py
+++
@@ -1,6 +0,0 @@
-def saveModel(model, iters, results_save_path):
-    model_json = model.to_json()
-    fp = open(results_save_path + '\\modelP.json','w')
-    fp.write(model_json)
-    # model.save(results_save_path + '\\model-{}.h5'.format(str(iters)))
-    model.save_weights(results_save_path + '\\modelW-{}.h5'.format(str(iters)))

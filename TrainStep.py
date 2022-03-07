6b3957650a2d0bb1546cb721d3ab25ad119c1851
raik7 <52767477+raik7@users.noreply.github.com>
2022-03-07 10:16:25 +0800

    Delete TrainStep.py

--- TrainStep.py
+++
@@ -1,9 +0,0 @@
-import evaluate
-
-def trainStep(model, X_train, Y_train, X_test, Y_test, epochs, batchSize, iters, results_save_path, losshistory, reverse = False):
-    for epoch in range(epochs):
-        print('Epoch : {}'.format(epoch+1))
-        model.fit(x=X_train, y=Y_train, batch_size=batchSize, epochs=1, verbose=1)
-        evaluate.evaluateModel(model,X_test, Y_test, batchSize, iters, results_save_path)
-
-    return model
